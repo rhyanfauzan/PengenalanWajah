1c94a0eeb6ab87b645ee038b436b386b593092b4
rhyanfauzan <rhyananiki@gmail.com>
2021-01-03 21:02:27 +0700

    Pengenalan Wajah

---
+++ webcam.py
@@ -0,0 +1,9 @@
+import cv2
+cam = cv2.VideoCapture(0)
+while True:
+    retV, frame = cam.read()
+    cv2.imshow('Webcamku',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
