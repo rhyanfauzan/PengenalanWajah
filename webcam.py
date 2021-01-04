9f24b810cb619bca917aba49450b33a68293c45a
rhyanfauzan <rhyananiki@gmail.com>
2021-01-04 11:26:59 +0700

    Penerapan Algoritma

--- webcam.py
+++ webcam.py
@@ -1,9 +1,18 @@
 import cv2
 cam = cv2.VideoCapture(0)
+cam.set(3, 640) #ubah lebar cam
+cam.set(4, 480) #ubah tinggi cam
+faceDetector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 while True:
     retV, frame = cam.read()
+    abuAbu = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceDetector.detectMultiScale(abuAbu, 1.3, 5) #frame, scaleFactor,
+    for (x,y,w,h) in faces:
+        frame = cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,255),2)
     cv2.imshow('Webcamku',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    #cv2.imshow('Webcam - Grey',abuAbu)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27 or k == ord('q'):
         break
 cam.release()
 cv2.destroyAllWindows()
